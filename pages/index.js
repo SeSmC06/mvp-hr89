6f5e845d83c10f6a2bb6f2497492315c60d73719
SeSmC06 <dingpeng06@gmail.com>
2018-03-31 14:08:27 -0700

    map rendered

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import Link from 'next/link';
+import GoogleMapReact from 'google-map-react';
+const { Link } = require('next/link');
 const { key: _APIKEY } = require('../apikey');
 
 export default class extends Component {
-  initmap = () => {
-    return new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
-    });
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
   };
+
+  onClick = ({ x, y, lat, lng, event }) =>
+    console.log(x, y, lat, lng, event);
+
   render() {
     return (
-      <div>
-        <h1>Welcome to Baby Wiper ${_APIKEY} Say hello Daddy</h1>
-        <div id="map" />
+      // Important! Always set the container height explicitly
+      <div style={{ height: '50vh', width: '50%vh' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: _APIKEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          onClick={this.onClick}
+        />
       </div>
     );
   }
